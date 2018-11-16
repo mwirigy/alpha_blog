aa76f4646cee8fe2c9abcf721050d8ef15dcba59
Charles Mwirigi <mwirigicharles@gmail.com>
2018-11-16 16:29:39 +0300

    Restrict action in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destory]
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
     end
     
     def new
@@ -15,7 +17,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain:params[:article].inspect
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success]= "Article was successfully created"
             redirect_to article_path(@article)
@@ -56,5 +58,11 @@ class ArticlesController < ApplicationController
             params.require(:article).permit(:title, :description)
         end
 
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own article"
+                redirect_to root_path
+            end
+        end
 
 end
